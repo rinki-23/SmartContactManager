0bac0c84457add07cfcf59c891e93f06ec3a9d1a
rinki <rinkimittal20062GAMIL.COM>
2026-09-17 08:29:45 +0530

    Initial Commit
    Merge branch 'master' of https://github.com/rinki-23/smartcontactmanager

--- src/main/java/com/smart/controller/UserController.java
+++ src/main/java/com/smart/controller/UserController.java
@@ -6,6 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
@@ -18,9 +19,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.smart.dao.ContactREpo;
 import com.smart.dao.UserRepo;
 import com.smart.entities.Contact;
 import com.smart.entities.User;
+import com.smart.helper.Message;
+
+import jakarta.servlet.http.HttpSession;
 
 
 
@@ -29,6 +34,9 @@ import com.smart.entities.User;
 public class UserController {
 	@Autowired
 	private UserRepo userRepo;
+	
+	@Autowired
+	private ContactREpo contactRepo;
 	
 	// work for all handler
 	@ModelAttribute
@@ -51,14 +59,16 @@ public class UserController {
 	}
 	
 	@GetMapping("/add-contact")
-	public String add(Model m) {
+	public String add(Model m, HttpSession session) {
+		// remove message
+		session.removeAttribute("message");
 		m.addAttribute("title", "Add Contact");
 		m.addAttribute("contact", new Contact());
 		return "norml/add-contact";
 	}
 	
 	@PostMapping("/process-contact")
-	public String savedata(@ModelAttribute Contact contact,@RequestParam("profileImage") MultipartFile file,  Principal principal) {
+	public String savedata(@ModelAttribute Contact contact,@RequestParam("profileImage") MultipartFile file,  Principal principal, HttpSession session) {
 		try {
 			// first we get the name of user
 			String name = principal.getName();
@@ -87,11 +97,37 @@ public class UserController {
 		userRepo.save(user);
 		
 		System.out.println("Added to data base");
+		
+		//message success
+		
+		session.setAttribute("message", new Message("Your contact is added successfully", "alert-success"));
 			
 		}catch(Exception e) {
 			e.printStackTrace();
 			System.out.println("Error" + e.getMessage());
+			// error message
+			session.setAttribute("message", new Message("Something went wrong", "alert-danger"));
 		}
 		return "norml/add-contact";
+	}
+	
+	@GetMapping("/view-contacts")
+	public String view(Model m, Principal principal) {
+		m.addAttribute("title", "View Contacts");
+		// first fetch the user name 
+		//String name = principal.getName();
+		// then fetch the user details from the databse
+		//User user = userRepo.getUserByUserName(name);
+		//List<Contact> contact = user.getContacts();
+		
+		// we want the contact of that user who is logged in
+		String name = principal.getName();
+		User user = userRepo.getUserByUserName(name);
+		
+		List<Contact> list = contactRepo.findContactsByUser(user.getId());
+		m.addAttribute("contacts", list);
+		
+		
+		return "norml/view";
 	}
 }
